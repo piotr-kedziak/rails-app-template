663d41f96ca56bc86ee399a41c3cd000c4b8eac3
Piotr Kędziak <piotr@kedziak.com>
2016-04-06 14:56:55 +0200

    create views

--- template.rb
+++ template.rb
@@ -44,6 +44,21 @@ inside 'app/helpers' do
   template 'nav_helper.rb'
 end
 
+say 'creating views and layouts'
+inside 'app/views' do
+  template 'application/btn/_back.html.erb'
+  template 'application/btn/_login.html.erb'
+  template 'application/_form_errors.html.erb'
+  template 'flash/_message.html.erb'
+  template 'flash/_notifications.html.erb'
+  template 'layouts/application.html.erb'
+  template 'layouts/base.html.erb'
+  template 'layouts/devise.html.erb'
+  template 'nav/_element.html.erb'
+  template 'nav/_header.html.erb'
+  template 'nav/_logout.html.erb'
+  template 'nav/_top.html.erb'
+end
 after_bundle do
   say 'initializeing git repository'
   git :init
