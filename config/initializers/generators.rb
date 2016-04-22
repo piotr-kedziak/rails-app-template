a0d0d2b33b88aebf19909b4db6084d17d6ca4567
Piotr Kędziak <piotr@kedziak.com>
2016-04-22 13:43:51 +0200

    Generators initializer refactoring

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -4,7 +4,13 @@ Rails.application.config.generators do |g|
   g.stylesheets           false
   g.javascripts           false
   g.helper                false
-  g.test_framework        :rspec
+  g.test_framework        :rspec,
+    fixtures:         true,
+    view_specs:       false,
+    helper_specs:     false,
+    routing_specs:    false,
+    controller_specs: false,
+    request_specs:    false
   g.factory_girl          dir: 'factories'
   g.scaffold_controller   :bootstrap_scaffold_controller
 end
